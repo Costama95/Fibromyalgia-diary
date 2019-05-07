9d5ed68413d4c929dbe97cfd1473d6346ac8d66c
Antongibbi <45274165+Antongibbi@users.noreply.github.com>
2019-05-07 14:52:14 +0300

    fix

--- HTML/signupfibro.php
+++ HTML/signupfibro.php
@@ -77,7 +77,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: /~jonathac/signinfibro.php");
+                header("location: signinfibro.php");
             } else{
                 echo "Something went wrong. Please try again later.";
             }
